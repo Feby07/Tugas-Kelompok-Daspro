f788dbb9c933833e81713d739af703028e285e33
Feby07 <rahmawatifeby5@gmail.com>
2024-09-07 14:27:54 +0700

    simpan kode java ke Github

---
+++ Athaa10.java
@@ -0,0 +1,18 @@
+package Tugas-Kelompok-Daspro;
+
+public class Athaa10 {
+    public static void main(String[] args) {
+
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--Data Mahasiswa jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama        : ATHA MAULIDIA");
+        System.out.println("NIM         : 244107060080");
+        System.out.println("Kelas       : 1C");
+        System.out.println("No Absen    : 03");
+        System.out.println("Prodi       : D-IV Sistem Informasi Bisnis");
+        System.out.println("Alamat      : Pati");
+        System.out.println("No HP       : 085952644284");
+        System.out.println("Hobi        : Bermain Game");
+    }
+}
