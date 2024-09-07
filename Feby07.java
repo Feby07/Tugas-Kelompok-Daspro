8975aedbe03339da8ed0481e836a5ef62451b507
Feby07 <rahmawatifeby5@gmail.com>
2024-09-07 13:43:50 +0700

    simpan kode java ke Github

---
+++ Feby07.java
@@ -0,0 +1,17 @@
+package TugasKelompokDaspro;
+
+public class Feby07 {
+    public static void main(String[] args){
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--Data Mahasiswa jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama        : FEBY RAHMAWATI AHMAD");
+        System.out.println("NIM         : 244107060139");
+        System.out.println("Kelas       : 1C");
+        System.out.println("No Absen    : 07");
+        System.out.println("Prodi       : D-IV Sistem Informasi Bisnis");
+        System.out.println("Alamat      : Lamongan");
+        System.out.println("No HP       : 085648198264");
+        System.out.println("Hobi        : Mendengarkan Lagu");
+    }
+}
